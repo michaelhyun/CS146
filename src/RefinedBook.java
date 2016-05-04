01955169de96f0250da84c7afc017ffd81c0d050
huangzdavid <huang.z.david@gmail.com>
2016-05-04 02:37:16 -0700

    Create RefinedBook.java

---
+++ src/RefinedBook.java
@@ -0,0 +1,25 @@
+public class RefinedBook{
+	private int index;
+	private int ref;
+
+	RefinedBook(){
+		index = 0;
+		ref = 0;
+	}
+
+	public void setIndex(int index){
+		this.index = index;
+	}
+
+	public void setRef(int ref){
+		this.ref = ref;
+	}
+
+	public int getIndex(){
+		return index;
+	}
+
+	public int getRef(){
+		return ref;
+	}
+}
